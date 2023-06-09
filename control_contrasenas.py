800b142218ea0c5fc4804a93a39b010f9793b887
David Leirado Maroto <dleiradomaroto@gmail.com>
2023-06-09 09:43:31 +0200

    funcion main para controlar la ejecucion creada

--- control_contrasenas.py
+++ control_contrasenas.py
@@ -11,3 +11,15 @@ def compara_contraseñas(contraseña_introducida, contraseña_correcta):
     return True if contraseña_introducida == contraseña_correcta else False
 
 #TODO: función main que controle el flujo del programa
+
+def main():
+    print("Bienvenido a mi programa")
+    print()
+    print("Introduzca la contraseña")
+    contraseña_usuario = input()
+    print()
+    
+    if compara_contraseñas(contraseña_usuario, contrasena):
+        print("Bienvenidx!!")
+    else:
+        print("MAAAAAAAAAL")
